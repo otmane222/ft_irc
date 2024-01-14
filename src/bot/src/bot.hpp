5a21a78107e048f5d2d47c4df67cbd49bfee9347
ISMAILBOUADDI <ismailbouaddi55@gmail.com>
2024-01-14 16:23:54 +0100

    new up

---
+++ src/bot/src/bot.hpp
@@ -0,0 +1,49 @@
+#ifndef BOT_HPP
+# define BOT_HPP
+
+# include <sys/socket.h>
+# include <netinet/in.h>
+# include <arpa/inet.h>
+# include <cerrno>
+# include <cstdio>
+# include <cstdlib>
+# include <cstring>
+# include <iostream>
+# include <unistd.h>
+# include <poll.h>
+# include <vector>
+# include <fstream>
+# include <algorithm>
+# include "Utils/Utils.hpp"
+
+#define DEBUG false
+
+class Bot
+{
+	private:
+		bool *sig;
+		std::string addr;
+		int sock;
+		struct sockaddr_in serv_addr;
+
+		std::string pass;
+		std::string nick;
+		bool connected;
+
+		std::string receive;
+		std::vector<std::string> messages;
+		std::ofstream log;
+
+		void send_msg(std::string msg);
+		void recv_msg();
+		void send_pass();
+		void send_nick();
+	public:
+		Bot(bool *sig, std::string addr, int port = 6667, std::string pass = std::string(), std::string nick = "bot");
+		~Bot();
+
+		void run();
+};
+
+
+#endif
